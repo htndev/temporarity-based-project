e9f78a0cf5ed1c60974a2cf2a4f325bf0eb5df4a
htndev <htn.developer@gmail.com>
2022-11-21 00:21:31 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [img, setImg] = useState("");
+
+  useEffect(() => {
+    axios
+      .get(
+        "http://localhost:4000/routes/uber/users/123/accept-request/positive",
+        {
+          withCredentials: true,
+          headers: {
+            "X-Temporarity-Api-Key":
+              "$2b$10$/YqvBUAZR.WowTJ7QPbQDuHHp8rtx7w4sdVgUiJb4I.TaaBJ/uZ4q",
+          },
+          responseType: "blob",
+        }
+      )
+      .then((res) => {
+        setImg(URL.createObjectURL(res.data));
+      });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <img src={img} />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
